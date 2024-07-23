61ea3622d666d15d7bba35e0f303a0ed61f85576
Pan <panqiwork@gmail.com>
2024-07-23 13:18:23 +0800

    libs.versions.toml添加bundle标签，简化依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,13 +58,9 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.bundles.camerax)
+    implementation(libs.bundles.essential)
 
 //    implementation(libs.cameraRecord)
-
     implementation(project(":camera_record"))
 }
--- camera_record/build.gradle.kts
+++ camera_record/build.gradle.kts
@@ -63,17 +63,8 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-
-    implementation(libs.androidx.camera.core)
-    implementation(libs.androidx.camera.camera2)
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.video)
-    implementation(libs.androidx.camera.view)
-    implementation(libs.androidx.camera.extensions)
+    implementation(libs.bundles.camerax)
+    implementation(libs.bundles.essential)
 
 }
 
